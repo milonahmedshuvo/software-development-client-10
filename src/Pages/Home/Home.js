10978b4c0b04c118f7269d47062854ecdfddbb33
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2022-10-27 21:39:49 +0600

    home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,7 +6,6 @@ const Home = () => {
       <div className='w-full'>
         <img src={'https://cdn2.psychologytoday.com/assets/styles/manual_crop_1_91_1_1528x800/public/2020-09/shutterstock_529299211.jpg?itok=MyhKRY7z'} alt="" className='w-full h-96 bg-no-repeat image-full'/>
 
-
         <div className='text-center mt-20'>
           <h1 className='w-1/2 mx-auto text-4xl font-extrabold text-orange-500 font-serif'>Top 6 Software Development Courses to Learn in 2022</h1>
           <p className='w-1/2 mx-auto mt-4'>In this blog, we will be discussing some of the most popular software development courses that you can learn to kick-start your career as a software developer. Furthermore, we will also discuss the different developer profiles and their future scope that might interest you after taking up those Software courses.</p>
